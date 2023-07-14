0f08b8c5887139f98a96b55efedbde4d05ce70df
MD Mehedi Hasan <md@bursement.com>
2023-07-15 02:21:09 +0600

    Users authentication apis setup and user reducer made in store

---
+++ src/redux/features/users/usersSlice.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+interface IUser {
+  user: {
+    email: string | null;
+  };
+  isLoading: boolean;
+  isError: boolean;
+  error: string | null;
+}
+
+const initialState: IUser = {
+  user: {
+    email: null,
+  },
+  isLoading: false,
+  isError: false,
+  error: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<string | null>) => {
+      state.user.email = action.payload;
+    },
+    setLoading: (state, action: PayloadAction<boolean>) => {
+      state.isLoading = action.payload;
+    },
+    setError: (state, action: PayloadAction<boolean>) => {
+      state.isError = action.payload;
+    },
+    setErrorMessage: (state, action: PayloadAction<string | null>) => {
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { setUser, setLoading, setError, setErrorMessage } =
+  userSlice.actions;
+
+export default userSlice.reducer;
